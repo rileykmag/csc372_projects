5a96231a578b83178a8f0aa659c11aa184755973
Riley Maguire <rileykmaguire@gmail.com>
2025-02-16 19:15:49 -0500

    Implement Bootstrap Framework

--- js/home.js
+++ js/home.js
@@ -1,14 +1,37 @@
-// get meeting button and message
-var questions = document.getElementsByClassName("questions");
-var meetmessage = document.getElementById("meetText");
+// // get meeting button and message
+// var questions = document.getElementsByClassName("questions");
+// var meetmessage = document.getElementById("meetText");
 
-// update meeting message when button is clicked
-questions[0].addEventListener("click", function() {
-    meetmessage.innerHTML = "We meet on Wednesdays from 5-6pm in Tyler Hall Room 055!";
-});
+// // update meeting message when button is clicked
+// questions[0].addEventListener("click", function() {
+//     meetmessage.innerHTML = "We meet on Wednesdays from 5-6pm in Tyler Hall Room 055!";
+// });
 
-// get contact message and update it when button is clicked
-var contactmessage = document.getElementById("contactText");
-questions[1].addEventListener("click", function() {
-    contactmessage.innerHTML = "You can contact us at uriswic@gmail.com and follow us on Instagram @uriswic!";
+// // get contact message and update it when button is clicked
+// var contactmessage = document.getElementById("contactText");
+// questions[1].addEventListener("click", function() {
+//     contactmessage.innerHTML = "You can contact us at uriswic@gmail.com and follow us on Instagram @uriswic!";
+// });
+
+// get message and update it when button is clicked
+function setActive(button) {
+    var buttons = document.querySelectorAll('.questions');
+    buttons.forEach(function(btn) {
+        btn.classList.remove('active');
+    });
+    button.classList.add('active');
+}
+
+// add event listeners for when the buttons are clicked
+var buttons = document.querySelectorAll('.questions');
+var question = document.getElementById('questionText');
+buttons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+        setActive(button);
+        if (index === 0) {
+            question.innerHTML = "We meet on Wednesdays from 5-6pm in Tyler Hall Room 055!";
+        } else if (index === 1) {
+            question.innerHTML = "You can contact us at uriswic@gmail.com and follow us on Instagram @uriswic!";
+        }
+    });
 });
